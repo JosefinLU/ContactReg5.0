51cf8a89d9c20c58f2dfa54076837e249a0ac241
Josefin Lundquist <miss_lundquist@outlook.com>
2021-09-15 15:43:54 +0200

    added class, service, jsonfile, controller for contact

---
+++ RegContact5.0/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.Json;
+
+namespace RegContact5._0.Models
+{
+    public class Contact
+    {
+        // properties  getters and setters
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Telephone { get; set; }
+
+        //Constructor
+        public Contact(string name, string title, string telephone)
+        {
+            this.Name = name;
+            this.Title = title;
+            this.Telephone = telephone;
+        }
+
+        //serialize the object to Json
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Contact>(this);
+        }
+    }
+}
